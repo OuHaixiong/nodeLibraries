d9773f4682545046e10dce962078e013d29e8b49
Bear <258333309@qq.com>
2017-09-21 11:41:06 +0800

    * 判断对象或数组是否存在某个key      * 删除文件夹并排除文件

--- Utils.js
+++ Utils.js
@@ -21,6 +21,11 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = {
+
+    /**
+     * 判断对象或数组是否存在某个key
+     *
+     */
 	isExistsKey : function (obj, key) {
         return obj.hasOwnProperty(key);
 	},
@@ -46,4 +51,62 @@ module.exports = {
             fs.unlinkSync(absolutePath);
         }
 	},
+
+    /**
+     * 递归删除文件夹或文件（同步删除），并排除不需要删除的文件
+     * @{string} absolutePath 文件或目录的绝对路径
+     * @{array} excludeFile 不删除的文件（数组，每项页是绝对路径，目前只支持根目录下的文件排除）
+     */
+    deleteFileExcludeFile : function (absolutePath, excludeFile) {
+        if (!fs.existsSync(absolutePath)) {
+            return;
+        }
+        var _this = this;
+        
+        if (fs.statSync(absolutePath).isDirectory()) { // 文件夹
+            var files = fs.readdirSync(absolutePath);
+            files.forEach(function (file, index) {
+                var currentPath = path.join(absolutePath, './', file);
+                if (fs.statSync(currentPath).isDirectory()) { // directory
+                    _this.deleteFileExcludeFile(currentPath);
+                } else { // file
+                    if (typeof(excludeFile) == 'undefined') {
+                        fs.unlinkSync(currentPath);
+                    } else {
+                        var length = excludeFile.length;
+                        var isDelete = true; // true ： 可以删除
+                        for (var i=0; i<length; i++) {
+                            if (excludeFile[i] == currentPath) {
+                                isDelete = false; // false ： 不可以删除
+                                break;
+                            }
+                        }
+                        if (isDelete) {
+                            fs.unlinkSync(currentPath);
+                        }
+                    }
+                }
+            });
+            if (typeof(excludeFile) == 'undefined') {
+                fs.rmdirSync(absolutePath);
+            }
+        } else { // 文件
+            if (typeof(excludeFile) == 'undefined') {
+                fs.unlinkSync(absolutePath);
+            } else {
+                var length = excludeFile.length;
+                var isDelete = true; // true ： 可以删除
+                for (var i=0; i<length; i++) {
+                    if (excludeFile[i] == absolutePath) {
+                        isDelete = false; // false ： 不可以删除
+                        break;
+                    }
+                }
+                if (isDelete) {
+                    fs.unlinkSync(absolutePath);
+                }
+            }
+        }
+    }
+
 };
--- test/testUtils.js
+++ test/testUtils.js
@@ -13,3 +13,11 @@ console.log(Utils.isExistsKey(obj, 'name'));
 
 var currentPath = path.join(__dirname, '123');
 Utils.deleteDirectoryFile(currentPath);
+
+var deletePath = path.join(__dirname, 'wokao');
+var excludeFile = [
+    path.join(deletePath, 'a.js'),
+    path.join(deletePath, 'b.php')
+];
+//Utils.deleteFileExcludeFile(deletePath, excludeFile);
+Utils.deleteFileExcludeFile(deletePath);
